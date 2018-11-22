0919e288a3b5cc55b7a02eb662124ee83b66843e
jonsumisu628 <ten_1106@icloud.com>
2018-11-22 23:22:52 +0900

    計算

---
+++ snake05.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import random
+x = 1
+y = 2
+z = x + y
+print(z)
+
+x = random.randint(1,100)
+y = random.randint(1,100)
+z = x + y
+print(z)
+
+# 和 = +   差 = -   積 = *   商 = /   余り = %
+# 式の優先度は数学と同じ(積、商が優先)
+# ()で閉じたらそっちが優先 
